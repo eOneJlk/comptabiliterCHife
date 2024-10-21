2b7a1ef2e879d33d2203e832776a561aa4caf1ce
eOneJlk <rohanrose_outlook.com>
2024-10-21 14:32:36 +0200

    creation de congee des agant

--- utilisateur/gerant/gerant.php
+++ utilisateur/gerant/gerant.php
@@ -62,7 +62,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['action']) && isset($_P
               <ul class="nav-links" style="display: flex; justify-content: space-between; align-items: center; padding: 0; margin: 0;">
                 <li style="list-style-type: none; margin: 10px;"><a href="./../../index.php" style="text-decoration: none; color: #333; font-weight: bold;">Dashboard</a></li>
                 <li style="list-style-type: none; margin: 10px;"><a href="personnel.php" style="text-decoration: none; color: #333; font-weight: bold;">Gestion du Personnel</a></li>
-                <li style="list-style-type: none; margin: 10px;"><a href="#dashboard-section" style="text-decoration: none; color: #333; font-weight: bold;">Graphiques</a></li>
+                <li style="list-style-type: none; margin: 10px;"><a href="form_congee.html" style="text-decoration: none; color: #333; font-weight: bold;">Congee Agant</a></li>
                 <div class="user-profile">
                 <img src="assets/img/logo_2.png" alt="User Avatar">
                 <span><a href="./../../profil.php"><?php echo isset($nom_complet) ? htmlspecialchars($nom_complet) : 'Utilisateur'; ?></a></span>
